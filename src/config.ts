40f6670071edf45398c1cee6315791f00bf14703
Lena L. <kisuni@gmail.com>
2024-02-06 23:14:34 +0300

    порт

--- src/config.ts
+++ src/config.ts
@@ -2,21 +2,22 @@ export const CONFIG = {
   payment: {
     //host: "http://127.0.0.1:3001/balance/reserve",
     //host: "http://payment-service.default.svc.cluster.local/balance/reserve"
-    host: process.env.PAYMENT_HOST ?? "",
+    host: process.env.PAYMENT_HOST ?? "http://127.0.0.1:3001/balance/reserve",
   },
   stock: {
     //host: "http://127.0.0.1:3002/products/reserve",
     //host: "http://stock-service.default.svc.cluster.local/products/reserve",
-    host: process.env.STOCK_HOST ?? "",
+    host: process.env.STOCK_HOST ?? "http://127.0.0.1:3002/products/reserve",
   },
   delivery: {
     //host: "http://127.0.0.1:3003/courier/reserve",
     //  host: "http://delivery-service.default.svc.cluster.local/courier/reserve",
-    host: process.env.DELIVERY_HOST ?? "",
+    host: process.env.DELIVERY_HOST ?? "http://127.0.0.1:3003/courier/reserve",
   },
   notify: {
     //host: "http://127.0.0.1:3004/message/send",
-    host: process.env.NOTIFY_HOST ?? "",
+    host: process.env.NOTIFY_HOST ?? "http://127.0.0.1:3004/message/send",
   },
   trace: false
 }
+
